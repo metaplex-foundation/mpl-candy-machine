66dce9daf53c33e30607da81afc5b3273b6d2b19
Loris Leiva <loris.leiva@gmail.com>
2023-03-02 11:43:38 +0000

    Finish first test

--- clients/js/test/initializeCandyMachine.test.ts
+++ clients/js/test/initializeCandyMachine.test.ts
@@ -2,11 +2,19 @@ import { createAccountWithRent } from '@metaplex-foundation/mpl-essentials';
 import { createNft } from '@metaplex-foundation/mpl-token-metadata';
 import {
   generateSigner,
+  none,
   percentAmount,
+  publicKey,
+  some,
   transactionBuilder,
 } from '@metaplex-foundation/umi';
 import test from 'ava';
-import { initializeCandyMachine } from '../src';
+import {
+  CandyMachine,
+  Creator,
+  fetchCandyMachine,
+  initializeCandyMachine,
+} from '../src';
 import { createUmi } from './_setup';
 
 /**
@@ -15,14 +23,14 @@ import { createUmi } from './_setup';
  */
 
 test('it can initialize a new candy machine account', async (t) => {
-  // Given an empty candy machine account.
+  // Given an empty candy machine account with a big enough size.
   const umi = await createUmi();
   const candyMachine = generateSigner(umi);
   await transactionBuilder(umi)
     .add(
       createAccountWithRent(umi, {
         newAccount: candyMachine,
-        space: 1000,
+        space: 5000,
         programId: umi.programs.get('mplCandyMachineCore').publicKey,
       })
     )
@@ -42,7 +50,7 @@ test('it can initialize a new candy machine account', async (t) => {
     )
     .sendAndConfirm();
 
-  // When
+  // When we initialize a candy machine at this address.
   const creator = generateSigner(umi);
   await transactionBuilder(umi)
     .add(
@@ -55,10 +63,49 @@ test('it can initialize a new candy machine account', async (t) => {
         creators: [
           { address: creator.publicKey, verified: false, percentageShare: 100 },
         ],
+        configLineSettings: some({
+          prefixName: 'My NFT #',
+          nameLength: 8,
+          prefixUri: 'https://example.com/',
+          uriLength: 20,
+          isSequential: false,
+        }),
       })
     )
     .sendAndConfirm();
 
-  // Then
-  t.pass();
+  // Then we expect the candy machine account to have the right data.
+  const candyMachineAccount = await fetchCandyMachine(
+    umi,
+    candyMachine.publicKey
+  );
+  t.like(candyMachineAccount, <CandyMachine>{
+    publicKey: publicKey(candyMachine),
+    authority: publicKey(umi.identity),
+    mintAuthority: publicKey(umi.identity),
+    collectionMint: publicKey(collectionMint),
+    itemsRedeemed: 0n,
+    data: {
+      itemsAvailable: 100n,
+      symbol: '',
+      sellerFeeBasisPoints: 500,
+      maxSupply: 0n,
+      isMutable: true,
+      creators: [
+        {
+          address: publicKey(creator),
+          verified: false,
+          percentageShare: 100,
+        },
+      ] as Creator[],
+      configLineSettings: some({
+        prefixName: 'My NFT #',
+        nameLength: 8,
+        prefixUri: 'https://example.com/',
+        uriLength: 20,
+        isSequential: false,
+      }),
+      hiddenSettings: none(),
+    },
+  });
 });

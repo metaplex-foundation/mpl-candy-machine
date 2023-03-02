d95ca3346d8a0d454f8dd172e7f09cfd8cc1e748
Loris Leiva <loris.leiva@gmail.com>
2023-03-02 09:45:37 +0000

    wip

--- clients/js/.eslintrc.js
+++ clients/js/.eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
     '@typescript-eslint/no-use-before-define': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
     'class-methods-use-this': 'off',
+    'import/no-cycle': 'off',
     'import/prefer-default-export': 'off',
     'no-underscore-dangle': 'off',
     'max-classes-per-file': 'off',
---
+++ clients/js/test/initializeCandyMachine.test.ts
@@ -0,0 +1,38 @@
+import { createAccountWithRent } from '@metaplex-foundation/mpl-essentials';
+import { generateSigner, transactionBuilder } from '@metaplex-foundation/umi';
+import test from 'ava';
+import { initializeCandyMachine } from '../src';
+import { createUmi } from './_setup';
+
+/**
+ * Note that most of the tests for the "initializeCandyMachine" instructions are
+ * part of the "createCandyMachine" tests as they are more convenient to test.
+ */
+
+test('it can initialize a new candy machine account', async (t) => {
+  // Given an empty candy machine account.
+  const umi = await createUmi();
+  const candyMachine = generateSigner(umi);
+  await transactionBuilder(umi)
+    .add(
+      createAccountWithRent(umi, {
+        newAccount: candyMachine,
+        space: 1000,
+        programId: umi.programs.get('mplCandyMachineCore').publicKey,
+      })
+    )
+    .sendAndConfirm();
+
+  // When
+  await transactionBuilder(umi)
+    .add(
+      initializeCandyMachine(umi, {
+        candyMachine: candyMachine.publicKey,
+        data: {},
+      })
+    )
+    .sendAndConfirm();
+
+  // Then
+  t.pass();
+});

22b095a41c36f6ea7116226c4bb3f7cb651a90d2
Loris Leiva <loris.leiva@gmail.com>
2023-03-02 11:30:52 +0000

    Add external programs

--- configs/validator.cjs
+++ configs/validator.cjs
@@ -4,6 +4,9 @@ const programDir = path.join(__dirname, "..", "programs");
 function getProgram(dir, programName) {
   return path.join(programDir, dir, "target", "deploy", programName);
 }
+function getExternalProgram(programName) {
+  return path.join(__dirname, "external-programs", programName);
+}
 
 module.exports = {
   validator: {
@@ -21,6 +24,21 @@ module.exports = {
         label: "Candy Guard",
         programId: "Guard1JwRhJkVH6XZhzoYxeBVQe872VH6QggF4BWmS9g",
         deployPath: getProgram("candy-guard", "mpl_candy_guard.so"),
+      },
+      {
+        label: "Token Metadata",
+        programId: "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s",
+        deployPath: getExternalProgram("mpl_token_metadata.so"),
+      },
+      {
+        label: "System Extras",
+        programId: "SysExL2WDyJi9aRZrXorrjHJut3JwHQ7R9bTyctbNNG",
+        deployPath: getExternalProgram("mpl_system_extras.so"),
+      },
+      {
+        label: "Token Extras",
+        programId: "TokExjvjJmhKaRBShsBAsbSvEWMA1AgUNK7ps4SAc2p",
+        deployPath: getExternalProgram("mpl_token_extras.so"),
       },
     ],
   },

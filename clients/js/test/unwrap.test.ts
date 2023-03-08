6ab7cd115d0399c66c3cd205e84833db68a7e7b6
Loris Leiva <loris.leiva@gmail.com>
2023-03-08 14:29:27 +0000

    wip

---
+++ clients/js/test/unwrap.test.ts
@@ -0,0 +1,26 @@
+import { publicKey, transactionBuilder } from '@metaplex-foundation/umi';
+import test from 'ava';
+import { CandyMachine, fetchCandyMachine, unwrap, wrap } from '../src';
+import { createCandyGuard, createCandyMachine, createUmi } from './_setup';
+
+test('it can unwrap a candy machine from its candy guard', async (t) => {
+  // Given an existing candy machine and a candy guard associated with it.
+  const umi = await createUmi();
+  const candyMachine = (await createCandyMachine(umi)).publicKey;
+  const candyGuard = await createCandyGuard(umi);
+  await transactionBuilder(umi)
+    .add(wrap(umi, { candyMachine, candyGuard }))
+    .sendAndConfirm();
+
+  // When we unwrap the candy machine from its candy guard.
+  await transactionBuilder(umi)
+    .add(unwrap(umi, { candyMachine, candyGuard }))
+    .sendAndConfirm();
+
+  // Then the mint authority of the candy machine was updated accordingly.
+  const candyMachineAccount = await fetchCandyMachine(umi, candyMachine);
+  t.like(candyMachineAccount, <CandyMachine>{
+    authority: publicKey(umi.identity),
+    mintAuthority: publicKey(umi.identity),
+  });
+});

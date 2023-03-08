c91e6e1a2f171ec1c4f6d610f58e357de1933ced
Loris Leiva <loris.leiva@gmail.com>
2023-03-08 15:39:04 +0000

    wip

--- clients/js/test/updateCandyGuard.test.ts
+++ clients/js/test/updateCandyGuard.test.ts
@@ -8,8 +8,10 @@ import {
 import test from 'ava';
 import {
   CandyGuard,
+  DefaultGuardSet,
   emptyDefaultGuardSetArgs,
   fetchCandyGuard,
+  GuardGroup,
   updateCandyGuard,
 } from '../src';
 import { createCandyGuard, createUmi } from './_setup';
@@ -96,5 +98,44 @@ test('it can update the guards of a candy guard', async (t) => {
         },
       },
     ],
+  });
+});
+
+test('it can remove all guards from a candy guard', async (t) => {
+  // Given an existing candy guard with defaults guards and groups.
+  const umi = await createUmi();
+  const destination = generateSigner(umi).publicKey;
+  const candyGuard = await createCandyGuard(umi, {
+    guards: {
+      botTax: some({ lamports: sol(0.01), lastInstruction: true }),
+    },
+    groups: [
+      {
+        label: 'OLD1',
+        guards: {
+          startDate: some({ date: '2022-09-13T10:00:00.000Z' }),
+          solPayment: some({ lamports: sol(2), destination }),
+        },
+      },
+      {
+        label: 'OLD2',
+        guards: {
+          startDate: some({ date: '2022-09-13T12:00:00.000Z' }),
+          solPayment: some({ lamports: sol(4), destination }),
+        },
+      },
+    ],
+  });
+
+  // When we update it so that it has no guards.
+  await transactionBuilder(umi)
+    .add(updateCandyGuard(umi, { candyGuard, guards: {}, groups: [] }))
+    .sendAndConfirm();
+
+  // Then all guards were removed as expected.
+  const candyGuardAccount = await fetchCandyGuard(umi, candyGuard);
+  t.like(candyGuardAccount, <CandyGuard>{
+    guards: emptyDefaultGuardSetArgs,
+    groups: [] as GuardGroup<DefaultGuardSet>[],
   });
 });

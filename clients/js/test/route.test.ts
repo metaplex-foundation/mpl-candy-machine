a1205bc03b0c3fd3069b0f71c2d745549032627d
Loris Leiva <loris.leiva@gmail.com>
2023-03-10 15:30:50 +0000

    wip

--- clients/js/test/route.test.ts
+++ clients/js/test/route.test.ts
@@ -1,31 +1,42 @@
-import { transactionBuilder } from '@metaplex-foundation/umi';
+import {
+  base58PublicKey,
+  generateSigner,
+  some,
+  transactionBuilder,
+} from '@metaplex-foundation/umi';
 import test from 'ava';
-import { route } from '../src';
+import { getMerkleProof, getMerkleRoot, route } from '../src';
 import { createUmi, createV2 } from './_setup';
 
 test('it can call the route instruction of a specific guard', async (t) => {
   // Given a candy machine with an allow list guard.
   const umi = await createUmi();
+  const minter = generateSigner(umi);
+  const allowedWallets = [
+    base58PublicKey(minter),
+    'Ur1CbWSGsXCdedknRbJsEk7urwAvu1uddmQv51nAnXB',
+    'GjwcWFQYzemBtpUoN5fMAP2FZviTtMRWCmrppGuTthJS',
+    '2vjCrmEFiN9CLLhiqy8u1JPh48av8Zpzp3kNkdTtirYG',
+  ];
+  const merkleRoot = getMerkleRoot(allowedWallets);
   const { publicKey: candyMachine } = await createV2(umi, {
-    //
+    guards: { allowList: some({ merkleRoot }) },
   });
 
   // When
+  const merkleProof = getMerkleProof(allowedWallets, base58PublicKey(minter));
   await transactionBuilder(umi)
     .add(
       route(umi, {
         candyMachine,
         guard: 'allowList',
-        routeArgs: {
-          path: 'proof',
-          merkleRoot: 'root',
-          merkleProof: 'proof',
-        },
+        routeArgs: { path: 'proof', merkleRoot, merkleProof },
       })
     )
     .sendAndConfirm();
 
   // Then
+  t.pass();
 });
 
 // Tests from JS SDK

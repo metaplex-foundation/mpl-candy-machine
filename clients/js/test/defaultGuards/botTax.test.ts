0d0bb8f65a413f13df939aac51507bbd61c17b60
Loris Leiva <loris.leiva@gmail.com>
2023-03-12 23:42:18 +0000

    Add botTax tests

---
+++ clients/js/test/defaultGuards/botTax.test.ts
@@ -0,0 +1,80 @@
+import {
+  addMemo,
+  setComputeUnitLimit,
+} from '@metaplex-foundation/mpl-essentials';
+import {
+  generateSigner,
+  sol,
+  some,
+  transactionBuilder,
+} from '@metaplex-foundation/umi';
+import test from 'ava';
+import { mintV2 } from '../../src';
+import {
+  assertBotTax,
+  assertSuccessfulMint,
+  createCollectionNft,
+  createUmi,
+  createV2,
+} from '../_setup';
+
+test('it does nothing if all conditions are valid', async (t) => {
+  // Given a candy machine with a bot tax guard.
+  const umi = await createUmi();
+  const collectionMint = (await createCollectionNft(umi)).publicKey;
+  const { publicKey: candyMachine } = await createV2(umi, {
+    collectionMint,
+    configLines: [{ name: 'Degen #1', uri: 'https://example.com/degen/1' }],
+    guards: {
+      botTax: some({ lamports: sol(0.01), lastInstruction: true }),
+    },
+  });
+
+  // When we mint from it.
+  const mint = generateSigner(umi);
+  await transactionBuilder(umi)
+    .add(setComputeUnitLimit(umi, { units: 600_000 }))
+    .add(
+      mintV2(umi, {
+        candyMachine,
+        nftMint: mint,
+        collectionMint,
+        collectionUpdateAuthority: umi.identity.publicKey,
+      })
+    )
+    .sendAndConfirm();
+
+  // Then the mint was successful.
+  await assertSuccessfulMint(t, umi, { mint, owner: umi.identity });
+});
+
+test('it optionally charges a bot tax if the mint instruction is not the last one', async (t) => {
+  // Given a candy machine with a bot tax guard with lastInstruction set to true.
+  const umi = await createUmi();
+  const collectionMint = (await createCollectionNft(umi)).publicKey;
+  const { publicKey: candyMachine } = await createV2(umi, {
+    collectionMint,
+    configLines: [{ name: 'Degen #1', uri: 'https://example.com/degen/1' }],
+    guards: {
+      botTax: some({ lamports: sol(0.01), lastInstruction: true }),
+    },
+  });
+
+  // When we try to mint from it whilst having more instructions after the mint instruction.
+  const mint = generateSigner(umi);
+  const { signature } = await transactionBuilder(umi)
+    .add(setComputeUnitLimit(umi, { units: 600_000 }))
+    .add(
+      mintV2(umi, {
+        candyMachine,
+        nftMint: mint,
+        collectionMint,
+        collectionUpdateAuthority: umi.identity.publicKey,
+      })
+    )
+    .add(addMemo(umi, { memo: 'I am a post-mint instruction' }))
+    .sendAndConfirm();
+
+  // Then we expect a silent bot tax error.
+  await assertBotTax(t, umi, mint, signature, /MintNotLastTransaction/);
+});

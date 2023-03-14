904277a65b43d776fcead585c3c0d3281ce95e35
Loris Leiva <loris.leiva@gmail.com>
2023-03-14 09:05:16 +0000

    add redeemedAmount tests

--- clients/js/test/defaultGuards/redeemedAmount.test.ts
+++ clients/js/test/defaultGuards/redeemedAmount.test.ts
@@ -1,164 +1,147 @@
-// import {
-//   generateSigner,
-//   sol,
-//   some,
-//   transactionBuilder,
-// } from '@metaplex-foundation/umi';
-// import test from 'ava';
-// import { createCollectionNft, createUmi, createV2 } from '../_setup';
-// import { mintV2 } from '../../src';
+import { setComputeUnitLimit } from '@metaplex-foundation/mpl-essentials';
+import {
+  generateSigner,
+  sol,
+  some,
+  transactionBuilder,
+} from '@metaplex-foundation/umi';
+import test from 'ava';
+import { mintV2 } from '../../src';
+import {
+  assertBotTax,
+  assertSuccessfulMint,
+  createCollectionNft,
+  createUmi,
+  createV2,
+} from '../_setup';
 
-// test('it allows minting until a threshold of NFTs have been redeemed', async (t) => {
-//   // Given a loaded Candy Machine with a redeemedAmount guard with a threshold of 1 NFT.
-//   const umi = await createUmi();
-//   const collectionMint = (await createCollectionNft(umi)).publicKey;
-//   const { publicKey: candyMachine } = await createV2(umi, {
-//     collectionMint,
+test('it allows minting until a threshold of NFTs have been redeemed', async (t) => {
+  // Given a loaded Candy Machine with a redeemedAmount guard with a threshold of 1 NFT.
+  const umi = await createUmi();
+  const collectionMint = (await createCollectionNft(umi)).publicKey;
+  const { publicKey: candyMachine } = await createV2(umi, {
+    collectionMint,
+    configLines: [
+      { name: 'Degen #1', uri: 'https://example.com/degen/1' },
+      { name: 'Degen #1', uri: 'https://example.com/degen/1' },
+    ],
+    guards: {
+      redeemedAmount: some({ maximum: 1 }),
+    },
+  });
 
-//     configLines: [
-//       { name: 'Degen #1', uri: 'https://example.com/degen/1' },
-//       { name: 'Degen #1', uri: 'https://example.com/degen/1' },
-//     ],
-//     guards: {
-//       redeemedAmount: {
-//         maximum: toBigNumber(1),
-//       },
-//     },
-//   });
+  // When we mint its first item.
+  const mint = generateSigner(umi);
+  await transactionBuilder(umi)
+    .add(setComputeUnitLimit(umi, { units: 600_000 }))
+    .add(
+      mintV2(umi, {
+        candyMachine,
+        nftMint: mint,
+        collectionMint,
+        collectionUpdateAuthority: umi.identity.publicKey,
+      })
+    )
+    .sendAndConfirm();
 
-//   // When we mint its first item.
-//   const payer = await generateSignerWithSol(umi, sol(10));
-//   const mint = generateSigner(umi);
-//   await transactionBuilder(umi).add().sendAndConfirm();
-//   mintV2(
-//     umi,
-//     {
-//       candyMachine,
-//       collectionUpdateAuthority: collection.updateAuthority.publicKey,
-//     },
-//     { payer }
-//   );
+  // Then minting was successful.
+  await assertSuccessfulMint(t, umi, { mint, owner: umi.identity });
+});
 
-//   // Then minting was successful.
-//   await assertSuccessfulMint(
-//     t,
-//     umi,
-//     { mint, owner: minter },
-//     {
-//       candyMachine,
-//       collectionUpdateAuthority: collection.updateAuthority.publicKey,
-//       nft,
-//       owner: payer.publicKey,
-//     }
-//   );
-// });
+test('it forbids minting once the redeemed threshold has been reached', async (t) => {
+  // Given a loaded Candy Machine with a redeemedAmount guard with a threshold of 1 NFT.
+  const umi = await createUmi();
+  const collectionMint = (await createCollectionNft(umi)).publicKey;
+  const { publicKey: candyMachine } = await createV2(umi, {
+    collectionMint,
+    configLines: [
+      { name: 'Degen #1', uri: 'https://example.com/degen/1' },
+      { name: 'Degen #1', uri: 'https://example.com/degen/1' },
+    ],
+    guards: {
+      redeemedAmount: some({ maximum: 1 }),
+    },
+  });
 
-// test('it forbids minting once the redeemed threshold has been reached', async (t) => {
-//   // Given a loaded Candy Machine with a redeemedAmount guard with a threshold of 1 NFT.
-//   const umi = await createUmi();
-//   const collectionMint = (await createCollectionNft(umi)).publicKey;
-//   const { publicKey: candyMachine } = await createV2(umi, {
-//     collectionMint,
+  // And assuming its first item has already been minted.
+  const mintA = generateSigner(umi);
+  await transactionBuilder(umi)
+    .add(setComputeUnitLimit(umi, { units: 600_000 }))
+    .add(
+      mintV2(umi, {
+        candyMachine,
+        nftMint: mintA,
+        collectionMint,
+        collectionUpdateAuthority: umi.identity.publicKey,
+      })
+    )
+    .sendAndConfirm();
+  await assertSuccessfulMint(t, umi, { mint: mintA, owner: umi.identity });
 
-//     configLines: [
-//       { name: 'Degen #1', uri: 'https://example.com/degen/1' },
-//       { name: 'Degen #1', uri: 'https://example.com/degen/1' },
-//     ],
-//     guards: {
-//       redeemedAmount: {
-//         maximum: toBigNumber(1),
-//       },
-//     },
-//   });
+  // When we try to mint its second item.
+  const mintB = generateSigner(umi);
+  const promise = transactionBuilder(umi)
+    .add(setComputeUnitLimit(umi, { units: 600_000 }))
+    .add(
+      mintV2(umi, {
+        candyMachine,
+        nftMint: mintB,
+        collectionMint,
+        collectionUpdateAuthority: umi.identity.publicKey,
+      })
+    )
+    .sendAndConfirm();
 
-//   // And assuming its first item has already been minted.
-//   const mint = generateSigner(umi);
-//   await transactionBuilder(umi).add().sendAndConfirm();
-//   mintV2(
-//     umi,
-//     {
-//       candyMachine,
-//       collectionUpdateAuthority: collection.updateAuthority.publicKey,
-//     },
-//     { payer: await generateSignerWithSol(umi, sol(10)) }
-//   );
+  // Then we expect a program error.
+  await t.throwsAsync(promise, { message: /MaximumRedeemedAmount/ });
+});
 
-//   // When we try to mint its second item.
-//   const payer = await generateSignerWithSol(umi, sol(10));
-//   const mint = generateSigner(umi);
-//   const promise = transactionBuilder(umi).add().sendAndConfirm();
-//   mintV2(
-//     umi,
-//     {
-//       candyMachine,
-//       collectionUpdateAuthority: collection.updateAuthority.publicKey,
-//     },
-//     { payer }
-//   );
+test('it charges a bot tax when trying to mint once the threshold has been reached', async (t) => {
+  // Given a loaded Candy Machine with a bot tax guard
+  // and a redeemedAmount guard with a threshold of 1 NFT.
+  const umi = await createUmi();
+  const collectionMint = (await createCollectionNft(umi)).publicKey;
+  const { publicKey: candyMachine } = await createV2(umi, {
+    collectionMint,
+    configLines: [
+      { name: 'Degen #1', uri: 'https://example.com/degen/1' },
+      { name: 'Degen #1', uri: 'https://example.com/degen/1' },
+    ],
+    guards: {
+      botTax: some({ lamports: sol(0.1), lastInstruction: true }),
+      redeemedAmount: some({ maximum: 1 }),
+    },
+  });
 
-//   // Then we expect an error.
-//   await assertThrows(
-//     t,
-//     promise,
-//     /Current redemeed items is at the set maximum amount/
-//   );
-// });
+  // And assuming its first item has already been minted.
+  const mintA = generateSigner(umi);
+  await transactionBuilder(umi)
+    .add(setComputeUnitLimit(umi, { units: 600_000 }))
+    .add(
+      mintV2(umi, {
+        candyMachine,
+        nftMint: mintA,
+        collectionMint,
+        collectionUpdateAuthority: umi.identity.publicKey,
+      })
+    )
+    .sendAndConfirm();
+  await assertSuccessfulMint(t, umi, { mint: mintA, owner: umi.identity });
 
-// test('it charges a bot tax when trying to mint once the threshold has been reached', async (t) => {
-//   // Given a loaded Candy Machine with a bot tax guard
-//   // and a redeemedAmount guard with a threshold of 1 NFT.
-//   const umi = await createUmi();
-//   const collectionMint = (await createCollectionNft(umi)).publicKey;
-//   const { publicKey: candyMachine } = await createV2(umi, {
-//     collectionMint,
+  // When we try to mint its second item.
+  const mintB = generateSigner(umi);
+  const { signature } = await transactionBuilder(umi)
+    .add(setComputeUnitLimit(umi, { units: 600_000 }))
+    .add(
+      mintV2(umi, {
+        candyMachine,
+        nftMint: mintB,
+        collectionMint,
+        collectionUpdateAuthority: umi.identity.publicKey,
+      })
+    )
+    .sendAndConfirm();
 
-//     configLines: [
-//       { name: 'Degen #1', uri: 'https://example.com/degen/1' },
-//       { name: 'Degen #1', uri: 'https://example.com/degen/1' },
-//     ],
-//     guards: {
-//       botTax: {
-//         lamports: sol(0.1),
-//         lastInstruction: true,
-//       },
-//       redeemedAmount: {
-//         maximum: toBigNumber(1),
-//       },
-//     },
-//   });
-
-//   // And assuming its first item has already been minted.
-//   const mint = generateSigner(umi);
-//   await transactionBuilder(umi).add().sendAndConfirm();
-//   mintV2(
-//     umi,
-//     {
-//       candyMachine,
-//       collectionUpdateAuthority: collection.updateAuthority.publicKey,
-//     },
-//     { payer: await generateSignerWithSol(umi, sol(10)) }
-//   );
-
-//   // When we try to mint its second item.
-//   const payer = await generateSignerWithSol(umi, sol(10));
-//   const mint = generateSigner(umi);
-//   const promise = transactionBuilder(umi).add().sendAndConfirm();
-//   mintV2(
-//     umi,
-//     {
-//       candyMachine,
-//       collectionUpdateAuthority: collection.updateAuthority.publicKey,
-//     },
-//     { payer }
-//   );
-
-//   // Then we expect a bot tax error.
-//   await t.throwsAsync(promise, { message: /CandyMachineBotTaxError/ });
-
-//   // And the payer was charged a bot tax.
-//   const payerBalance = await umi.rpc.getBalance(payer.publicKey);
-//   t.true(
-//     isEqualToAmount(payerBalance, sol(9.9), sol(0.01)),
-//     'payer was charged a bot tax'
-//   );
-// });
+  // Then we expect a silent bot tax error.
+  await assertBotTax(t, umi, mintB, signature, /MaximumRedeemedAmount/);
+});

68819ff0c25afb5f16644d852b495c398e25e3f1
Loris Leiva <loris.leiva@gmail.com>
2023-03-31 10:01:47 +0100

    Fix Solitarc for CG and update IDL

--- clients/js-solita-candy-guard/.solitarc.js
+++ clients/js-solita-candy-guard/.solitarc.js
@@ -1,6 +1,6 @@
 // @ts-check
 const path = require('path');
-const programDir = path.join(__dirname, '..', '..', 'programs', 'candy-guard');
+const programDir = path.join(__dirname, '..', '..', 'programs', 'candy-guard', 'program');
 const idlDir = path.join(__dirname, 'idl');
 const sdkDir = path.join(__dirname, 'src', 'generated');
 const binaryInstallDir = path.join(__dirname, '..', '..', '.crates');

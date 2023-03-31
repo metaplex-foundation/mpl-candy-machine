0de0b4872d1a6d676f66ca1debd57a9266bd24c8
Loris Leiva <loris.leiva@gmail.com>
2023-03-31 10:03:30 +0100

    Fix Solitarc for CM and update IDL

--- clients/js-solita-candy-machine-core/.prettierrc.js
+++ clients/js-solita-candy-machine-core/.prettierrc.js
@@ -1,1 +1,11 @@
-module.exports = require('../../.prettierrc');
+module.exports = {
+  bracketSpacing: true,
+  bracketSameLine: false,
+  jsxSingleQuote: true,
+  printWidth: 100,
+  proseWrap: 'always',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+};
--- clients/js-solita-candy-machine-core/.solitarc.js
+++ clients/js-solita-candy-machine-core/.solitarc.js
@@ -1,9 +1,9 @@
 // @ts-check
 const path = require('path');
-const programDir = path.join(__dirname, '..', 'program');
+const programDir = path.join(__dirname, '..', '..', 'programs', 'candy-machine-core', 'program');
 const idlDir = path.join(__dirname, 'idl');
 const sdkDir = path.join(__dirname, 'src', 'generated');
-const binaryInstallDir = path.join(__dirname, '.crates');
+const binaryInstallDir = path.join(__dirname, '..', '..', '.crates');
 
 const idlHook = (idl) => {
     idl.instructions.map(ix => {

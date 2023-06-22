83b98a7713ea474c6bc40252e5f159f97394e55b
febo <febo@metaplex.com>
2023-06-22 11:17:07 +0100

    New binary location

--- configs/validator.cjs
+++ configs/validator.cjs
@@ -1,11 +1,9 @@
 const path = require("path");
 
 const programDir = path.join(__dirname, "..", "programs");
-function getProgram(dir, programName) {
-  return path.join(programDir, dir, "target", "deploy", programName);
-}
-function getExternalProgram(programName) {
-  return path.join(__dirname, "external-programs", programName);
+
+function getProgram(programName) {
+  return path.join(programDir, ".bin", programName);
 }
 
 module.exports = {
@@ -16,45 +14,42 @@ module.exports = {
       {
         label: "Candy Machine Core",
         programId: "CndyV3LdqHUfDLmE5naZjVN8rBZz4tqhdefbAnjHG3JR",
-        deployPath: getProgram(
-          "candy-machine-core",
-          "mpl_candy_machine_core.so"
-        ),
+        deployPath: getProgram("mpl_candy_machine_core.so"),
       },
       {
         label: "Candy Guard",
         programId: "Guard1JwRhJkVH6XZhzoYxeBVQe872VH6QggF4BWmS9g",
-        deployPath: getProgram("candy-guard", "mpl_candy_guard.so"),
+        deployPath: getProgram("mpl_candy_guard.so"),
       },
       {
         label: "Token Metadata",
         programId: "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s",
-        deployPath: getExternalProgram("mpl_token_metadata.so"),
+        deployPath: getProgram("mpl_token_metadata.so"),
       },
       {
         label: "Token Auth Rules",
         programId: "auth9SigNpDKz4sJJ1DfCTuZrZNSAgh9sFD3rboVmgg",
-        deployPath: getExternalProgram("mpl_token_auth_rules.so"),
+        deployPath: getProgram("mpl_token_auth_rules.so"),
       },
       {
         label: "System Extras",
         programId: "SysExL2WDyJi9aRZrXorrjHJut3JwHQ7R9bTyctbNNG",
-        deployPath: getExternalProgram("mpl_system_extras.so"),
+        deployPath: getProgram("mpl_system_extras.so"),
       },
       {
         label: "Token Extras",
         programId: "TokExjvjJmhKaRBShsBAsbSvEWMA1AgUNK7ps4SAc2p",
-        deployPath: getExternalProgram("mpl_token_extras.so"),
+        deployPath: getProgram("mpl_token_extras.so"),
       },
       {
         label: "Civic Gateway",
         programId: "gatem74V238djXdzWnJf94Wo1DcnuGkfijbf3AuBhfs",
-        deployPath: getExternalProgram("civic_gateway.so"),
+        deployPath: getProgram("civic_gateway.so"),
       },
       {
         label: "SPL Token 2022",
         programId: "TokenzQdBNbLqP5VEhdkAS6EPFLC1PHnBqCXEpPxuEb",
-        deployPath: getExternalProgram("spl_token_2022.so"),
+        deployPath: getProgram("spl_token_2022.so"),
       },
     ],
     accounts: [

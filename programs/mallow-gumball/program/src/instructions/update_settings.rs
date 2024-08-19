891fd257c4bcc390b39c3626af870f38e0f8b819
Austin Borden <austin.borden@gmail.com>
2024-08-19 11:48:16 +0700

    allow updating hide sold items when in solo mode

--- programs/mallow-gumball/program/src/instructions/update_settings.rs
+++ programs/mallow-gumball/program/src/instructions/update_settings.rs
@@ -22,27 +22,33 @@ pub fn update_settings(ctx: Context<UpdateSettings>, settings: GumballSettings) 
     let gumball_machine = &mut ctx.accounts.gumball_machine;
     let account_info = gumball_machine.to_account_info();
     let account_data = account_info.data.borrow_mut();
-    let config_count = get_config_count(&account_data)? as u64;
+    let items_loaded = get_config_count(&account_data)? as u64;
 
     // uri and sellers_merkle_root can always be changed
 
     // TODO: Allow decreasing capacity
     if settings.item_capacity != gumball_machine.settings.item_capacity {
+        msg!("Cannot update item capacity");
         return err!(GumballError::InvalidSettingUpdate);
     }
 
     // Limit the possible updates when details are finalized or there are already items loaded
-    if gumball_machine.state == GumballState::DetailsFinalized || config_count > 0 {
+    if gumball_machine.state == GumballState::DetailsFinalized || items_loaded > 0 {
         // Can only increase items_per_seller
         if settings.items_per_seller < gumball_machine.settings.items_per_seller {
+            msg!("Cannot decrease items_per_seller");
             return err!(GumballError::InvalidSettingUpdate);
         }
         // Can only decrease curator fee bps
         if settings.curator_fee_bps > gumball_machine.settings.curator_fee_bps {
+            msg!("Cannot increase curator_fee_bps");
             return err!(GumballError::InvalidSettingUpdate);
         }
-        // Cannot change hide_sold_items
-        if settings.hide_sold_items != gumball_machine.settings.hide_sold_items {
+
+        // Cannot change hide_sold_items if others have been invited
+        if gumball_machine.settings.sellers_merkle_root.is_some() &&
+            settings.hide_sold_items != gumball_machine.settings.hide_sold_items {
+            msg!("Cannot change hide_sold_items");
             return err!(GumballError::InvalidSettingUpdate);
         }
     }
